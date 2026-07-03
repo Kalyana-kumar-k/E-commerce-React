b660a36007a2209be9b622ccd3bbc25d78056ff0
Kalyana kumar <kalyankutty1994@gmail.com>
2026-07-03 18:02:17 +0530

    basename change

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
   return (
     <UserContext.Provider value={{ user }}>
       <Fragment>
-        <Router>
+        <Router basename="/E-commerce-React">
           {/* <ol>
             <li>
               <Link to={"/"}>Home</Link>
